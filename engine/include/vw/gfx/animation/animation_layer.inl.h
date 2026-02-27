9a6100244e3c8f3bb47fa43044724e3511af6f13
wiredreamer <34518940+wiredreamer@users.noreply.github.com>
2026-02-27 15:49:16 +0300

    engine: animation blending fix

--- engine/include/vw/gfx/animation/animation_layer.inl.h
+++ engine/include/vw/gfx/animation/animation_layer.inl.h
@@ -3,7 +3,10 @@
 namespace vw::gfx {
 
 inline auto animation_layer::is_active() const -> bool {
-    return state == animation_state::playing || fade_is_out;
+    if (state == animation_state::playing || fade_is_out) {
+        return true;
+    }
+    return blend_transition.duration > 0.0f && blend_elapsed < blend_transition.duration;
 }
 
 inline auto animation_layer::affects_target(const std::string& name) const -> bool {
