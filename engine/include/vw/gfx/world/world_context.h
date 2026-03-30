be4225565bb49a4a89e6f7430ccbc858fd1c25f7
LuciusMortified <leo.fed97@gmail.com>
2026-03-30 16:47:10 +0300

    engine: small refactoring

--- engine/include/vw/gfx/world/world_context.h
+++ engine/include/vw/gfx/world/world_context.h
@@ -18,34 +18,41 @@ template <typename WC>
 class world;
 
 template <typename WC>
-using world_grid_ptr = std::shared_ptr<world_grid<WC>>;
-
-template <typename WC>
 class world_context {
-    friend class world<WC>;
-
 public:
-    using registry_type = typename entity_registry_from_tuple<WC>::type;
-    using grid_ptr = world_grid_ptr<WC>;
+    using registry_type   = entity_registry_from_tuple<WC>::type;
+    using world_grid_type = world_grid<WC>;
 
-    explicit world_context(registry_type& reg)
-        : registry_(&reg) {}
-
-    world_context(registry_type& reg, class mesh_pool* mp, grid_ptr wg = {})
+    world_context(
+        registry_type& reg, mesh_pool* mp, std::shared_ptr<world_grid_type> wg = nullptr
+    )
         : registry_(&reg), mesh_pool_(mp), world_grid_(std::move(wg)) {}
 
-    [[nodiscard]] auto registry() -> registry_type& { return *registry_; }
-    [[nodiscard]] auto registry() const -> const registry_type& { return *registry_; }
-    [[nodiscard]] auto mesh_pool() -> class mesh_pool* { return mesh_pool_; }
-    [[nodiscard]] auto mesh_pool() const -> const class mesh_pool* { return mesh_pool_; }
-    [[nodiscard]] auto world_grid() -> grid_ptr& { return world_grid_; }
-    [[nodiscard]] auto world_grid() const -> const grid_ptr& { return world_grid_; }
+    [[nodiscard]] auto registry() -> registry_type& {
+        return *registry_;
+    }
+    [[nodiscard]] auto registry() const -> const registry_type& {
+        return *registry_;
+    }
+    [[nodiscard]] auto mesh_pool() -> mesh_pool* {
+        return mesh_pool_;
+    }
+    [[nodiscard]] auto mesh_pool() const -> const class mesh_pool* {
+        return mesh_pool_;
+    }
+    [[nodiscard]] auto world_grid() -> std::shared_ptr<world_grid_type> {
+        return world_grid_;
+    }
+    [[nodiscard]] auto world_grid() const -> std::shared_ptr<world_grid_type> {
+        return world_grid_;
+    }
 
 private:
+    friend class world<WC>;
 
-    registry_type* registry_ = nullptr;
+    registry_type* registry_    = nullptr;
     class mesh_pool* mesh_pool_ = nullptr;
-    grid_ptr world_grid_;
+    std::shared_ptr<world_grid_type> world_grid_;
 };
 
 }  // namespace vw::gfx
